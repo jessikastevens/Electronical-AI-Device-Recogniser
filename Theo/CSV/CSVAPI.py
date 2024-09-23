6bc8f78ec7118da7d8e7935b7821dcd394a9badf
23knightst813 <144670355+23knightst813@users.noreply.github.com>
2024-09-23 13:52:21 +0100

    Added

--- Theo/CSV/CSVAPI.py
+++ Theo/CSV/CSVAPI.py
@@ -1,6 +1,18 @@
 from flask import Flask, jsonify, request
 import time
 import json
+import pandas as pd 
+
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def api():
+    
 
 
 
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
