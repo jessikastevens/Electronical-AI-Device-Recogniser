9758c6f605067faeb72cbaada2ef0ae4d659354f
23kngightst813 <23kngightst813@collyers.ac.uk>
2024-09-23 15:03:26 +0100

    early version of khanyas ai api

--- Theo/CSV/CSVAPI.py
+++ Theo/CSV/CSVAPI.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def api():
-    
+    print()
 
 
 
--- Theo/Logic/LogicAPI.py
+++ Theo/Logic/LogicAPI.py
@@ -1,27 +1,53 @@
-from flask import Flask, jsonify, request , json
-import time
+from flask import Flask, jsonify, request
+import json
 import pandas as pd 
-
+import os
+import requests
 
 app = Flask(__name__)
 
-@app.route('/CSV', methods=['GET', 'POST'])
-def api():
-    print('Start CSV')
-
-@app.route('/AI', methods=['GET', 'POST'])
-def api():
-    print('StartAI')
-    if request == ['POST']:
-        print('Post')
-
+@app.route('/AI', methods=['GET'])
+def ai_route():
+    if request.method == 'GET':
+        print('StartAI')
+        data = request.get_json()
         
+        real_power = data.get('Real Power')
+        reactive_power = data.get('Reactive Power')
+        rms_current = data.get('RMS Current')
+        frequency = data.get('Frequency')
+        rms_voltage = data.get('RMS Voltage')
+        phase_angle = data.get('Phase Angle')
+        
+        ordered_array = [frequency, phase_angle, real_power, reactive_power, rms_current, rms_voltage]
+        
+        # Convert list to JSON
+        data_json = json.dumps(ordered_array)
 
+        # # Send GET request
+        # url = os.environ.get('AI_API_URL')
+        # response = requests.get(url, json=data_json)
+
+        # Return to front end
+        return data_json
+
+@app.route('/CSV', methods=['GET', 'POST'])
+def csv_route():
+    print('StartCSV')
+    if request.method == 'POST':
+        print('Post')
     
-
-
-
-
+        data = request.get_json()
+    
+        appliance_type = data.get('Appliance')
+        date_1 = data.get('Begin Date')
+        date_2 = data.get('Ending Date')
+        
+        # Process the data here
+        
+        return jsonify({"message": "Data received successfully"})
+    
+    return jsonify({"error": "Method not allowed"}), 405
 
 if __name__ == '__main__':
     app.run(debug=True)
