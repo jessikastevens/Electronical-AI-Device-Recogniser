5164432e48681b82bafe503e87e661bd248d3efd
23kngightst813 <23kngightst813@collyers.ac.uk>
2024-09-23 13:52:21 +0100

    Added

---
+++ Theo/Logic/LogicAPI.py
@@ -0,0 +1,24 @@
+from flask import Flask, jsonify, request , json
+import time
+import pandas as pd 
+
+
+app = Flask(__name__)
+
+@app.route('/CSV', methods=['GET', 'POST'])
+def api():
+    print('Start CSV')
+
+@app.route('/AI', methods=['GET', 'POST'])
+def api():
+    print('StartAI')
+    if request == ['POST']
+
+    
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
