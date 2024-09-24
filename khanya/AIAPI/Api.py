5522b7d911156e38bef66530814c86014104fd22
khanya-mcfadden <23malgasmk037@collyers.ac.uk>
2024-09-24 15:34:49 +0100

    Add appliance_terms and appliance_tags dictionaries to LogicAPI.py

--- khanya/AIAPI/Api.py
+++ khanya/AIAPI/Api.py
@@ -1,6 +1,7 @@
 import random
 from flask import Flask, jsonify, request
 import numpy as np
+from tensorflow.keras.models import load_model
 
 app = Flask(__name__)
 
@@ -8,8 +9,19 @@ app = Flask(__name__)
 def api():
     data = request.get_json()
 
-    # TODO: Implement actual AI prediction logic here
-    # This is a dummy response for now
+    # Load your pre-trained model (this is just an example, adjust according to your model and framework)
+
+    model = load_model('path_to_your_model.h5')
+
+    # Preprocess the input data as required by your model
+    input_data = np.array(data['input'])  # Assuming the input data is in the 'input' field of the JSON
+    input_data = input_data.reshape((1, -1))  # Reshape if necessary
+
+    # Make a prediction using the model
+    raw_prediction = model.predict(input_data)[0]
+
+    # Get the index of the highest value of raw_prediction
+    predicted_class = int(np.argmax(raw_prediction))
 
     # Generate random raw prediction (10 items with positive probabilities that sum up to 1)
     raw_prediction = np.random.dirichlet(np.ones(15))
