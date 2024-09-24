b7736eb4a1c2ca1a0aa2322363d7cc24263a957e
23knightst813 <144670355+23knightst813@users.noreply.github.com>
2024-09-24 10:15:34 +0100

    Finished Theo Apis

--- Theo/CSV/CSVAPI.py
+++ Theo/CSV/CSVAPI.py
@@ -1,18 +1,34 @@
 from flask import Flask, jsonify, request
-import time
-import json
-import pandas as pd 
-
+import pandas as pd
+from datetime import datetime
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['POST'])
 def api():
-    print()
+    data = pd.read_csv('Theo/CSV/acs-f2-dataset 1.csv')
+    data['time'] = pd.to_datetime(data['time'])
 
+    request_data = request.get_json()
 
+    equipment = request_data.get('appliance_type')
+    start_date = pd.to_datetime(request_data.get('start'))
+    end_date = pd.to_datetime(request_data.get('end'))
 
+    filtered_data = data[(data['time'] >= start_date) & 
+                         (data['time'] <= end_date) & 
+                         (data['equipment'] == equipment)]
+    
+    columns = ['time', 'rmsVolt', 'rmsCur', 'power', 'reacPower']
+    filtered_data = filtered_data[columns]
+
+    response = {
+        "columns": columns,
+        "data": filtered_data.values.tolist() 
+    }
+
+    return jsonify(response)
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True,port=7000)
