ed31b6493f8f9ece2bae30b0be1e05510865a12f
khanya-mcfadden <23malgasmk037@collyers.ac.uk>
2024-09-25 14:41:42 +0100

    Refactor data visualization code to use scatter plot for Current against Time

--- khanya/data managment/datasets/data-visual.py
+++ khanya/data managment/datasets/data-visual.py
@@ -2,13 +2,12 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-# Read the CSV file into a DataFrame
-df = pd.read_csv(r'c:/Users/honey/Documents/placment work/Electronical-AI-Device-Recogniser/khanya/data managment/acs-f2-dataset.csv')
-
+# Read the first 1000 rows of the CSV file into a DataFrame
+df = pd.read_csv(r'c:/Users/honey/Documents/placment work/Electronical-AI-Device-Recogniser/khanya/data managment/acs-f2-dataset.csv', nrows=500)
 
 # Plot Current against Time
 plt.figure(figsize=(10, 6))
-plt.scatter(df.iloc[:, 6], df.iloc[:, 5], color='orange', marker='x')
+plt.scatter(df.iloc[:, 4], df.iloc[:, 5], color='orange',edgecolors='black', linewidth=0.5)
 plt.title('Current vs Time')
 plt.xlabel('Time')
 plt.ylabel('Current')
