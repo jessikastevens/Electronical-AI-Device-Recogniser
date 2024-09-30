dfef6edb9947f7a7a7d854bc286f63fbd8c1dbe4
23knightst813 <144670355+23knightst813@users.noreply.github.com>
2024-09-30 10:02:02 +0100

    Update author info in update_author_info.sh script

--- update_author_info.sh
+++ update_author_info.sh
@@ -1,10 +1,10 @@
 git filter-branch -f --env-filter '
-if [ "$GIT_AUTHOR_EMAIL" = "144670355+23knightst813@users.noreply.github.com" ];
+if [ "$GIT_AUTHOR_NAME" = "23knightst813" ];
 then
-    GIT_AUTHOR_EMAIL="23knightst813@collyers.ac.uk";
+    GIT_AUTHOR_EMAIL="144670355+23knightst813@users.noreply.github.com";
 fi
-if [ "$GIT_COMMITTER_EMAIL" = "144670355+23knightst813@users.noreply.github.com" ];
+if [ "$GIT_COMMITTER_NAME" = "23knightst813" ];
 then
-    GIT_COMMITTER_EMAIL="23knightst813@collyers.ac.uk";
+    GIT_COMMITTER_EMAIL="144670355+23knightst813@users.noreply.github.com";
 fi
 ' --tag-name-filter cat -- --branches --tags
