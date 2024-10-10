d193976ab5fd13914feb18403b9d3c0dc40014b7
23knightst813 <23knightst813@collyers.ac.uk>
2024-10-10 14:23:46 +0100

    Changed back gradio port thingy

--- Jess/main.py
+++ Jess/main.py
@@ -341,4 +341,4 @@ with gr.Blocks(css=css, theme=theme_choice) as demo:
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
-    demo.launch(server_port=port)
+    demo.launch(server_port=port, server_name="0.0.0.0")
